7d84239d3c9dedc885e2464160c0834793107ff9
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 15:30:53 -0300

    Início em Python
    conhecendo tipos e váriaveis

--- Tipos_Dados/Inicio_Dados.py
+++ Tipos_Dados/Inicio_Dados.py
@@ -1,7 +1,9 @@
-# Tipos int, float e type
+# Principais tipos de dados
+# Tipos int, float, boolean e type
 """
 INT é a representação de números inteiros, sejam eles positivos ou negativos.
 FLOAT é a representação de números decimais, sejam eles positivos ou negativos.
+BOOLEAN é quando só existem duas respostas possíveis: sim (True) ou não (False)
 TYPE mostra o tipo que o python inferiu ao valor.
 """
 # Exemplos:
@@ -11,6 +13,8 @@ print(-11) #Inteiro negativo
 print(0) #Inteiro positivo
 print(1.1, 10.11) #Float positivo
 print(0.0, -1.5) #Float negativo
+print(10 == 10) #Boolean - sim - True (verdadeiro)
+print(10 == 11) #Boolean - não - False (falso) 
 print(type('Dayene')) #String
 print(type(0)) #Inteiro positivo
 print(type(1.1), type(-1.1), type(0.0)) #Inteiro positivo, inteiro negativo e inteiro positivo
