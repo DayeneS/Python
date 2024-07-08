04efc52b51d78e74b95269b53eac5df1b7bc185d
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 13:38:25 -0300

    Alteração de definição
    Atualizei de maneira objetiva uma simples definição do uso de DocString e #

--- Python.py
+++ Python.py
@@ -1,15 +1,14 @@
 """
-DocString
-E escrever o que eu
-quiser
-
-
-asdfasdfd
+DocString:
+Usar as três aspas duplas antes e depois de determinado texto
+permite escrever o que precisar que a linguagem irá "ignorar" os comandos, 
+e esse espaço pode ser usado como uma documentação.
 """
-
-''' Usar para escrever suas notas '''
-
-# Permite escrever um comentário
-print(123)  # Na frente
-# Abaixo
-print(456)
+'''
+O uso de aspas simples pode ser usado da mesma maneira, mas lembrando que 
+devemos manter um padrão, ou seja, definir o uso de aspas duplas ou simples.
+'''
+# Mensagem precedida de hastag não é reproduzida no código; ela pode ser usada antes,
+print(123) # depois do código,
+# ou abaixo
+print(456) 
