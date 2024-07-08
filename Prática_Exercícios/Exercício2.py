de7dcf60a513ec9e90199ca89cc1673d5f63c6b5
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 16:43:43 -0300

    Exercício 2
    Prática de calculo de imc

---
+++ Prática_Exercícios/Exercício2.py
@@ -0,0 +1,10 @@
+#Defina nome, altura e peso para as variáveis e calcule o IMC.
+
+nome = 'Dayene Santos'
+altura = 1.64
+peso = 48
+imc = peso / (altura*altura)
+
+print(nome, 'tem', altura, 'de altura',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
