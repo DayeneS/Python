2a1417c3d93e51b55a98ff6c8d8d246401f8b5b1
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 16:17:43 -0300

    Exercício 1
    Primeiros passos em python

---
+++ Prática_Exercícios/Exercício1.py
@@ -0,0 +1,21 @@
+"""
+Escreva as váriaveis corretas e seus respectivos valores para que obtenhamos os resultados:
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+"""
+nome = 'Dayene'
+sobrenome = 'Santos'
+idade = 27
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >=18
+altura_metros = 1.64
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
