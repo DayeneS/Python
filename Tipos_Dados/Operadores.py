c7f3ffca73f7cd3e801d387d49f6cdbee04fb093
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 15:43:42 -0300

    Operadores
    conhecendo os tipos de operadores

---
+++ Tipos_Dados/Operadores.py
@@ -0,0 +1,22 @@
+#Operadores lógicos e relacionais
+"""
+Operadores matemáticos:
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+** Exponenciação
+% Módulo - retorna o resto de uma divisão não exata.
+
+Operador de atribuição:
+=
+# x = 10 se lê 'x recebe o valor 10'
+
+Operadores relacionais:
+== Igual
+!= Diferente
+> Maior
+< Menor
+>= Maior ou igual
+<= Menor ou igual
+"""
