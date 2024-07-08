c7f0bb5748e106d56e907b76a98f352b4dd6b6c3
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 14:44:33 -0300

    Tipos de dados numéricos
    definição e exemplos dos primeiros tipos de dados numéricos apresentados

---
+++ Tipos_Dados/Dados.py
@@ -0,0 +1,17 @@
+# Tipos int, float e type
+"""
+INT é a representação de números inteiros, sejam eles positivos ou negativos.
+FLOAT é a representação de números decimais, sejam eles positivos ou negativos.
+TYPE mostra o tipo que o python inferiu ao valor.
+"""
+# Exemplos:
+print(11) #Inteiro positivo
+print(-11) #Inteiro negativo
+print(0) #Inteiro positivo
+print(1.1, 10.11) #Float positivo
+print(0.0, -1.5) #Float negativo
+print(type('Dayene')) #String
+print(type(0)) #Inteiro positivo
+print(type(1.1), type(-1.1), type(0.0)) #Inteiro positivo, inteiro negativo e inteiro positivo
+
+
