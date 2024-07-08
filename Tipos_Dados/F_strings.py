a79e8c6f3899f7e1ea8073cf8c1349de596aa3bd
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 18:42:09 -0300

    F_Strings
    Atualização de exemplo

--- Tipos_Dados/F_strings.py
+++ Tipos_Dados/F_strings.py
@@ -15,3 +15,18 @@ linha_3 = f'e seu imc é de {imc}'
 print(linha_1)
 print(linha_2)
 print(linha_3)
+
+a = 'AAAA'
+b = 'BBBB'
+c = 1.1
+#string = 'a={} b={} c={:.2f}'
+#string = 'a={0} b={1} c={2:.2f}'
+string = 'b={1} a={0} a={0} a={0} c={2:.2f}'
+formato = string.format(a, b, c)
+#print(formato)
+#ou
+#string = 'a={} b={} c={:.2f}'
+string = 'a={nome1} b={nome2} c={nome3:.2f}'
+#string = 'b={1} a={0} a={0} a={0} c={nome3:.2f}'
+formato = string.format(nome1=a, nome2=b, nome3=c)
+print(formato)
