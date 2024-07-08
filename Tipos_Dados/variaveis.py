991c7519481d03561655344bd9e55ca0f6e25e6d
DayeneSantos <dayenesantos.work@gmail.com>
2024-07-08 15:52:52 -0300

    Variaveis
    Conhecendo as variáveis

---
+++ Tipos_Dados/variaveis.py
@@ -0,0 +1,13 @@
+"""
+Variáveis são usadas para salvar algo na memória do computador.
+O sinal de '=' é o operador de atribuição. Ele é usado para
+atribuir um valor a um nome (variável).
+Uso: nome_variavel = 2 + 1
+Exemplo:
+nome_completo = 'Dayene dos Santos Rosa'
+soma_dois_mais_dois = 2 + 2
+int_um = bool ('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+"""
